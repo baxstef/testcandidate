7dd2c743c2c3745049cacb6b81e16db0150862ba
Stefano Barbato <barbato.stefano@hotmail.it>
2024-05-01 21:25:30 +0200

    edit exception to delete degree used

--- app/Models/Degree.php
+++ app/Models/Degree.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Exceptions\ProtectedDegreeException;
 
 
 class Degree extends Model
@@ -17,5 +18,22 @@ class Degree extends Model
     public function candidates()
     {
         return $this->hasMany(Candidate::class);
+    }
+
+    public function preventDeleteIfUsed()
+    {
+        if ($this->candidates()->exists()) {
+            throw new ProtectedDegreeException('This degree is in use by a candidate and cannot be deleted.');
+        }
+    }
+
+    // Sovrascrivi il metodo delete per eseguire la logica di verifica
+    public function delete()
+    {
+        // Esegui la verifica prima di eliminare il grado
+        $this->preventDeleteIfUsed();
+
+        // Se non ci sono candidati associati, procedi con l'eliminazione
+        return parent::delete();
     }
 }
